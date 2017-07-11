6b63036c97a5ef7ad245336bd638f534dc9e6152
adaniluk <allldaniluk@gmail.com>
2017-07-11 10:27:28 +0300

    'JaggedArrayExtension' added

---
+++ Logic.Tests/JaggedArrayExtensionTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+using Logic;
+
+namespace Logic.Tests
+{
+    [TestFixture]
+    public class JaggedArrayExtensionTests
+    {
+        [Test]
+        public void BubbleSortAscElemSum_CorrectValues_PositiveTest()
+        {
+            int[][] arr = { new int[] {1,2,3}, new int[] {10}, new int[] {8} };
+            int[][] expected = { new int[] {1,2,3}, new int[] {8}, new int[] {10} };
+            JaggedArrayExtension.BubbleSorting(arr, JaggedArrayExtension.BubbleSortAscElemSum);
+            Assert.AreEqual(arr, expected);
+        }
+
+        [Test]
+        public void BubbleSortDescElemSum_CorrectValues_PositiveTest()
+        {
+            int[][] arr = { new int[] { 1, 2, 3 }, new int[] { 10 }, new int[] { 8 } };
+            int[][] expected = { new int[] { 10 }, new int[] { 8 }, new int[] { 1,2,3 } };
+            JaggedArrayExtension.BubbleSorting(arr, JaggedArrayExtension.BubbleSortDescElemSum);
+            Assert.AreEqual(arr, expected);
+        }
+
+        [Test]
+        public void BubbleSortAscMaxElem_CorrectValues_PositiveTest()
+        {
+            int[][] arr = { new int[] { 1, 15, 11 }, new int[] { 6, 2 }, new int[] { 8 } };
+            int[][] expected = { new int[] { 6, 2 }, new int[] { 8 }, new int[] { 1, 15, 11 } };
+            JaggedArrayExtension.BubbleSorting(arr, JaggedArrayExtension.BubbleSortAscMaxElem);
+            Assert.AreEqual(arr, expected);
+        }
+
+        [Test]
+        public void BubbleSortDescMaxElem_CorrectValues_PositiveTest()
+        {
+            int[][] arr = { new int[] { 1,5 }, new int[] { 13,2 }, new int[] { 14 } };
+            int[][] expected = { new int[] { 14 }, new int[] { 13,2 }, new int[] { 1,5 } };
+            JaggedArrayExtension.BubbleSorting(arr, JaggedArrayExtension.BubbleSortDescMaxElem);
+            Assert.AreEqual(arr, expected);
+        }
+
+        [Test]
+        public void BubbleSortAscMinElem_CorrectValues_PositiveTest()
+        {
+            int[][] arr = { new int[] { 1, 15, 11 }, new int[] { 6, 4 }, new int[] { 2,8 } };
+            int[][] expected = { new int[] { 1,15,11 }, new int[] { 2,8 }, new int[] { 6,4 } };
+            JaggedArrayExtension.BubbleSorting(arr, JaggedArrayExtension.BubbleSortAscMinElem);
+            Assert.AreEqual(arr, expected);
+        }
+
+        [Test]
+        public void BubbleSortDescMinElem_CorrectValues_PositiveTest()
+        {
+            int[][] arr = { new int[] { 1, 5 }, new int[] { 13, 4 }, new int[] { 2,14 } };
+            int[][] expected = { new int[] { 13,4 }, new int[] { 2,14 }, new int[] { 1,5 } };
+            JaggedArrayExtension.BubbleSorting(arr, JaggedArrayExtension.BubbleSortDescMinElem);
+            Assert.AreEqual(arr, expected);
+        }
+    }
+}
---
+++ Logic/JaggedArrayExtension.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+
+namespace Logic
+{
+    public static class JaggedArrayExtension
+    {
+        /// <summary>
+        /// Delegate for possible methods to deal with jagged array.
+        /// </summary>
+        /// <param name="lhse">One row in jagged array.</param>
+        /// <param name="rhs">Another row in jagged array.</param>
+        /// <returns>True, if conditions are met, and false otherwise.</returns>
+        public delegate bool VariantOfSort(int[] lhs, int[] rhs);
+
+        /// <summary>
+        /// Sort jagged array using bubble sorting by different criteria.
+        /// </summary>
+        /// <param name="array">Jagged array.</param>
+        /// <param name="variantOfSort">Selected method among suitable.</param>
+        public static void BubbleSorting(int[][] array, VariantOfSort variantOfSort)
+        {
+            CheckJaggedArray(array);
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (!variantOfSort(array[j], array[j + 1]))
+                    {
+                        Swap(array, j, j + 1);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// In the order of increasing sums of elements of rows in matrix.
+        /// </summary>
+        /// <param name="lhs">One row of matrix.</param>
+        /// <param name="rhs">Another row of matrix.</param>
+        /// <returns>True, if sum of the next row is greater, and false otherwise.</returns>
+        public static bool BubbleSortAscElemSum(int[] lhs, int[] rhs) => lhs.Sum() < rhs.Sum();
+
+        /// <summary>
+        /// In the order of decreasing sums of elements of rows in matrix.
+        /// </summary>
+        /// <param name="lhs">One row of matrix.</param>
+        /// <param name="rhs">Another row of matrix.</param>
+        /// <returns>True, if sum of the next row is less, and false otherwise.</returns>
+        public static bool BubbleSortDescElemSum(int[] lhs, int[] rhs) => lhs.Sum() > rhs.Sum();
+
+        /// <summary>
+        /// In the order of increasing max elements of rows in matrix.
+        /// </summary>
+        /// <param name="lhs">One row of matrix.</param>
+        /// <param name="rhs">Another row of matrix.</param>
+        /// <returns>True, if max element of the next row is greater, and false otherwise.</returns>
+        public static bool BubbleSortAscMaxElem(int[] lhs, int[] rhs) => lhs.Max() < rhs.Max();
+
+        /// <summary>
+        /// In the order of decreasing max elements of rows in matrix.
+        /// </summary>
+        /// <param name="lhs">One row of matrix.</param>
+        /// <param name="rhs">Another row of matrix.</param>
+        /// <returns>True, if max element of the next row is less, and false otherwise.</returns>
+        public static bool BubbleSortDescMaxElem(int[] lhs, int[] rhs) => lhs.Max() > rhs.Max();
+
+        /// <summary>
+        /// In the order of increasing min elements of rows in matrix.
+        /// </summary>
+        /// <param name="lhs">One row of matrix.</param>
+        /// <param name="rhs">Another row of matrix.</param>
+        /// <returns>True, if min element of the next row is greater, and false otherwise.</returns>
+        public static bool BubbleSortAscMinElem(int[] lhs, int[] rhs) => lhs.Min() < rhs.Min();
+
+        /// <summary>
+        /// In the order of decreasing min elements of rows in matrix.
+        /// </summary>
+        /// <param name="lhs">One row of matrix.</param>
+        /// <param name="rhs">Another row of matrix.</param>
+        /// <returns>True, if min element of the next row is less, and false otherwise.</returns>
+        public static bool BubbleSortDescMinElem(int[] lhs, int[] rhs) => lhs.Min() > rhs.Min();
+
+        private static void Swap(int[][] array, int i, int j)
+        {
+            int[] temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        private static void CheckJaggedArray(int[][] array)
+        {
+            if (ReferenceEquals(array, null)) throw new ArgumentNullException($"{nameof(array)} is null.");
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (ReferenceEquals(array[i], null)) throw new ArgumentNullException($"One of elements in {nameof(array)} is null.");
+            }
+        }
+    }
+}
